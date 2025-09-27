e4194530ebf74e8f411cb70e3212645dbc966957
oricardos <ricardo.rs95@gmail.com>
2025-09-27 15:25:01 -0300

    exercício modelagem de dados

--- commands.sql
+++ commands.sql
@@ -237,5 +237,63 @@ VALUES
     (1, 1);
 
 -- Default (ver tabela anterior)
-
 -- MODELAGEM DE DADOS (ver Readme)
+-------------- EXERCÍCIO 2 ----------------------
+CREATE TABLE
+    users (
+        id INTEGER PRIMARY KEY, --x NOT NULL
+        name TEXT NOT NULL,
+        password TEXT NOT NULL,
+        email TEXT NOT NULL COLLATE NOCASE UNIQUE, --COLLATE NOCASE UNIQUE
+        created TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP -- NOT NULL
+    ) STRICT;
+
+CREATE TABLE
+    courses (
+        id INTEGER PRIMARY KEY, --xNOT NULL
+        slug TEXT NOT NULL COLLATE NOCASE UNIQUE, --COLLATE NOCASE
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        lessons INTEGER NOT NULL,
+        hours INTEGER NOT NULL,
+        created TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) STRICT;
+
+CREATE TABLE
+    lessons (
+        id INTEGER PRIMARY KEY, --x NOT NULL
+        course_id INTEGER NOT NULL,
+        slug TEXT NOT NULL COLLATE NOCASE, --COLLATE NOCASE  xUNIQUE
+        title TEXT NOT NULL,
+        materia TEXT NOT NULL,
+        materia_slug TEXT NOT NULL,
+        seconds INTEGER NOT NULL,
+        video TEXT NOT NULL,
+        description TEXT NOT NULL,
+        lesson_order INTEGER NOT NULL,
+        free INTEGER NOT NULL DEFAULT 0 CHECK (free IN (0, 1)),
+        created TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (course_id) REFERENCES courses (id), --
+        UNIQUE (course_id, slug) --
+    ) STRICT;
+
+CREATE TABLE lessons_completed (
+  user_id INTEGER NOT NULL,
+  course_id INTEGER NOT NULL,
+  lesson_id INTEGER NOT NULL,
+  completed TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, course_id, lesson_id),
+  FOREIGN KEY (lesson_id) REFERENCES lessons (id),
+  FOREIGN KEY (course_id) REFERENCES courses (id),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+) STRICT;
+
+CREATE TABLE certificates (
+  id TEXT PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  course_id INTEGER NOT NULL,
+  completed TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (user_id, course_id),
+  FOREIGN KEY (course_id) REFERENCES courses (id),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+) STRICT;
