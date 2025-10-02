177589ed72cf1d9e9c8bfdd9825513db1b032623
Luiz Ricardo Silva <46203839+oricardos@users.noreply.github.com>
2025-10-02 16:25:48 -0300

    SUBQUERY E WITH AS

--- commands.sql
+++ commands.sql
@@ -1008,3 +1008,13 @@ GROUP BY
     categoria
 HAVING
     preco_medio > 70000;
+
+-- SUBQUERY --
+SELECT * FROM produtos WHERE preco > (SELECT AVG(preco) FROM produtos);
+SELECT * FROM lessons WHERE course_id = (SELECT id FROM courses WHERE slug = 'javascript-basico');
+
+-- WITH AS --
+WITH preco_medio AS (
+    SELECT AVG(preco) AS media FROM produtos
+)
+SELECT * FROM produtos WHERE preco > (SELECT media FROM preco_medio); 
